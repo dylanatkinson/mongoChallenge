8a0a96a4168bc6ae57eb04b46dece303492c7ff3
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-02-25 17:05:54 +0000

    connection added

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const express = require('express');
 const hbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+// const User = require('./models/user');
 
 const app = express();
 require('dotenv').config();
 
-mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@usersignup-j1kc2.mongodb.net/userdb?retryWrites=true&w=majority`, {
+mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster1-oi5ib.mongodb.net/test?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -27,6 +28,23 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.post('/', async (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password
+    console.log(username);
+    console.log(password);
+
+    // creates new user in our database
+
+    // const user = new User({
+    //     username: username,
+    //     password: password
+    // });
+    
+    // user.save();
+});
+
+
 app.listen(3000, () => {
-    console.log("server listening on port 3000"); 
+    console.log("server listening on port 3000");
 });
