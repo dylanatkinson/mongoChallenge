4b1b75fb70fce20991815a1f450e40dd12030e31
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-02-26 16:44:39 +0000

    checks if user has signed up yet

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const express = require('express');
 const hbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-// const User = require('./models/user');
+const getUsers = require('./lib/getUsers');
 
 const app = express();
 require('dotenv').config();
 
-mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster1-oi5ib.mongodb.net/test?retryWrites=true&w=majority`, {
+mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster1-oi5ib.mongodb.net/test?retryWrites=false&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -25,23 +25,22 @@ app.engine('.hbs', hbs({
 app.set('view engine', '.hbs');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { title: "UK Weather App - Login/Signup" });
 });
 
 app.post('/', async (req, res) => {
     let username = req.body.username;
     let password = req.body.password
-    console.log(username);
-    console.log(password);
 
-    // creates new user in our database
+    let docs = await getUsers(username, password);
 
-    // const user = new User({
-    //     username: username,
-    //     password: password
-    // });
-    
-    // user.save();
+    console.log(docs);
+
+    if (docs.length > 0) {
+        res.render('index', {err: "A user with this username already exists."});
+        return;
+    }
+    res.render('signup');
 });
 
 
